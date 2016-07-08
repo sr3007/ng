bb0ddfc66f5bb918d065d947c51aa73eb5a3370a
stiyyagu <stiyyagu@capgemini.com>
2016-07-08 18:17:29 -0400

    Updated

---
+++ ng_Route/ng_Route/Scripts/LogicGoesHere.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="angular.js" />
+/// <reference path="angular-route.js" />
+
+//ng app
+
+var myApp = angular.module("ngApp", ['ngRoute']);
+
+var controllers = {};
+
+controllers.firstCtrl = function ($scope) {
+    $scope.heading = "Example Route";
+
+    $scope.customers = [
+        { fistName: 'Srikanth', lastName: 'Tiyyaguru' },
+        { fistName: 'Mayukh', lastName: 'Tiyyaguru' },
+        { fistName: 'Sireesha', lastName: 'Konda' }
+    ];
+}
+
+myApp.controller(controllers);
+
+myApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'V1.html'
+        })
+        .when('/V2', {
+            templateUrl: 'V2.html'
+        })
+        .otherwise({ redirectTo: '/' })
+});
